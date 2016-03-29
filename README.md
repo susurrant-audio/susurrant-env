ac277a193cdb535901b4a5b5359b5b5fdff57d2d
Cora Johnson-Roberson <me@corajr.com>
2016-03-29 15:16:42 -0400

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# susurrant-env
+
+To run, first install [Vagrant](https://www.vagrantup.com/downloads.html) and [VirtualBox](https://www.virtualbox.org/wiki/Downloads), then:
+
+```shell
+vagrant up
+vagrant ssh
+cd /vagrant/py
+make
+```
---
+++ provision.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get install -y build-essential libyaml-dev libfftw3-dev libavcodec-dev libavformat-dev \
+     libavutil-dev libavresample-dev python-dev libsamplerate0-dev libtag1-dev \
+     git python-pip \
+     python-numpy-dev python-numpy python-yaml
+
+install_essentia() {
+    cd ~
+    git clone https://github.com/MTG/essentia.git
+    cd essentia
+    ./waf configure --mode=release --build-static --with-python --with-cpptests --with-examples --with-vamp --with-gaia
+    ./waf
+    sudo ./waf install
+}
+
+python -c 'import essentia' || install_essentia
+pip install -r /vagrant/py/requirements.txt
+
