8e37a4befdba1e00f93135b0b9c91062f807e0c9
Cora Johnson-Roberson <me@corajr.com>
2016-03-29 16:02:55 -0400

    don't cd twice

--- provision.sh
+++ provision.sh
@@ -9,11 +9,11 @@ sudo apt-get install -y build-essential libyaml-dev libfftw3-dev libavcodec-dev 
 install_essentia() {
     cd ~
     if cd essentia; then
-        git checkout v2.1_beta2;
+        git checkout v2.1_beta2
     else
-        git clone -b v2.1_beta2 https://github.com/MTG/essentia.git;
+        git clone -b v2.1_beta2 https://github.com/MTG/essentia.git
+        cd essentia
     fi
-    cd essentia
     ./waf configure --mode=release --build-static --with-python --with-cpptests --with-examples --with-vamp && \
         ./waf && \
         sudo ./waf install
